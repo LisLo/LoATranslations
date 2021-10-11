2321018e804e19928fe42204bdcc86a62e2fff81
David <lisaloerinci@gmail.com>
2021-10-11 12:08:59 +0200

    first steps

--- Main/main.py
+++ Main/main.py
@@ -1,5 +1,17 @@
 import os
 import sys
+from typing import ContextManager, get_type_hints
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
 # find root dir
 root_path = os.path.split((os.path.dirname(__file__)))[0]
 # set root
@@ -7,18 +19,84 @@ sys.path.append(root_path)
 from Source.Config.get_input import GetInput
 from Source.Services.my_math_methods import MyMethods
 
+Window.size = (1000, 700)
+Builder.load_string("""
 
-class Main(GetInput):
-    def execute_workflow(self):
-        print("Addition:")
-        print(MyMethods.addition(self.a, self.b))
-        print("Substraction:")
-        print(MyMethods.substraction(self.a, self.b))
-        print("a hoch b")
-        print(MyMethods.pows(self.a, self.b))
-        print("Square")
-        print(MyMethods.square(self.a))
+<Login>:
+    ben: benName.text
+    pw: passwort.text
+    knopf: btn
 
+    GridLayout:
+        cols: 1
+        size: root.width, root.height
+        GridLayout:
+            cols: 2
+            Label:
+                text: "Benutzername"
+                font_size: 30
+            TextInput:
+                id: benName
+                multiline: False
+                font_size: 30
+            Label:
+                text: "Passwort"
+                font_size: 30
+            TextInput:
+                password: True
+                id: passwort
+                multiline: False
+                font_size: 30
+        Button:
+            text: "anmelden"
+            id: btn
+            size_hint: (1., 0.5)
+            font_size: 30
+            on_release: 
+                root.loginPopup()
+                root.manager.current = "geheim" if passwort.text == "1234" and benName.text == "Python" else "login"
+                root.manager.transition.direction = "left"
 
-my_code = Main(__file__)
-my_code.execute_workflow()
+<geheimerBereich>:
+    BoxLayout:
+        orientation: "vertical"
+        Label:
+            text: "Geheimer Bereich"
+            font_size: 30
+        Button:
+            text: "zurück"
+            font_size: 30
+            on_release:
+                root.manager.current = "login"
+                root.manager.transition.direction = "right"
+""")
+
+class Login(Screen):
+    ben = StringProperty()
+    pw = StringProperty()
+    knopf = ObjectProperty()
+    
+    def loginPopup(self):
+        if self.ben == "" or self.pw == "":
+            popup = Popup(title="Fehler", 
+            content = Label(text="Es wurde kein Passwort oder Benutzername eingegeben!"),
+            size_hint=(None,None), size=(400,400))
+            popup.open()
+        else: 
+            if self.ben == "Python" and self.pw == "1234":
+                self.knopf.background_color = [0., 1., 0., 1.]
+            else:
+                self.knopf.background_color = [1., 0., 0., 1.]
+
+class geheimerBereich(Screen):
+    pass
+
+ms = ScreenManager()
+ms.add_widget(Login(name="login"))
+ms.add_widget(geheimerBereich(geheimerBereich="geheim"))
+class StartApp(App):
+    def build(self):
+        return ms
+
+if __name__ == "__main__":
+    StartApp().run()
